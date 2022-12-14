69958824dab12d67e4a00fa7fe71aca2f7e2d2f2
marshtv <marshtv.1972@gmail.com>
2022-12-15 01:18:07 +0300

    Continue

--- main.cpp
+++ main.cpp
@@ -12,15 +12,28 @@ bool sector_check(std::vector<int> sectors_complete, int sector) {
 }
 
 int main() {
-	int sectors_complete[13] = {0};
 	std::ifstream questions;
 	questions.open("F:\\__C++\\Skillbox\\19\\19.5.5\\Files\\_questions.txt");
 
-	std::cout << "To define a sector type the offset from current position of drum (from 1 to 12):" << std::endl;
+	std::vector<int> sectors_complete;
 	int offset;
-	std::cin >> offset;
+	int sector_count = 13;
+	int sector = 1;
+	while (sector_count != 0) {
+		std::cout << "To define a sector type the offset from current position of drum (from 1 to 12):" << std::endl;
+		std::cin >> offset;
+		sector += offset;
+		if (sector > 13) sector -= 13;
+		int count = sector_count;
+		while (sector_check(sectors_complete, sector) || count > 0) {
+			sector++;
+			count--;
+		}
 
+		sector_count--;
+	}
 
 	questions.close();
+
 	return 0;
 }
